641fdc3c85537877d9db6399ab54fb3c247749ec
Dawn Marble <mzsunrize@gmail.com>
2021-05-12 22:52:01 -0700

    Foundation and first test

---
+++ APIAutomationSample/Features/PostAPost.feature
@@ -0,0 +1,12 @@
+﻿Feature: Posts
+ In order to make sure I can interact with Posts via the JSONPlaceholder API
+ As a consumer of this API
+ I want to be sure I am able to receive the correct response when I send request
+
+@Smoke-HappyPath
+Scenario: Can post valid post
+	Given a <happypath> request body
+	When I POST the request
+	Then the response should be a 201 ("Created") response
+	And the response body should be what I expect
+	
---
+++ APIAutomationSample/Steps/PostSteps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using TechTalk.SpecFlow;
+using FluentAssertions;
+
+namespace APIAutomationSample.Steps
+{
+    [Binding]
+    public class PostSteps
+    {
+        private HttpResponseMessage result;
+        HttpClient client = new HttpClient();
+
+        [Given(@"a (.*) request body")]
+        public void GivenARequestBody(string type)
+        {
+            //take request 'type' JsonBody from Payloads
+
+            //returns jsonBody
+        }
+
+        [When(@"I POST the request")]
+        public async void WhenIPostTheRequest(string jsonBody)
+        {
+            var content = new StringContent(jsonBody, System.Text.Encoding.UTF8, "application/json");
+            result = await client.PostAsync("https://jsonplaceholder.typicode.com/posts/1", content);
+        }
+
+        [Then(@"the response should be a (.*) \(""(.*)""\) response")]
+        public void ThenTheResponseShouldBeAResponse(int code, string description)
+        {
+            result.StatusCode.Should().Be(code);
+            result.ReasonPhrase.Should().Be(description);
+        }
+
+        [Then(@"the response body should be what I expect")]
+        public void ThenTheResponseBodyShouldBeWhatIExpect()
+        {
+            result.Content.ToString().Should().Contain("id");
+            //parse the Json response and assert each property
+        }
+
+
+    }
+}
